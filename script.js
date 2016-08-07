4746b75f68992f5b68f51ff716b17939b0644a19
mabech <marcia.abech@gmail.com>
2016-08-06 21:35:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('So you want to learn about Marcia? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
